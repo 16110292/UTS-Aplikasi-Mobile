f76875cdf412f49f4ba11e7e36bd318420f02435
16110292 <39641031+16110292@users.noreply.github.com>
2018-11-01 21:35:23 +0700

    Tudas Responsi
    Responsi PAM

---
+++ app/src/main/java/moviez/mnf/com/movie/Activity/Config.java
@@ -0,0 +1,11 @@
+package moviez.mnf.com.movie.Activity;
+
+/**
+ * Created by Muneef on 17/05/15.
+ */
+public class Config {
+
+    public static final String YOUTUBE_API_KEY = "676a53fa9be26d5a8ea100d979f0342b";
+
+
+}
